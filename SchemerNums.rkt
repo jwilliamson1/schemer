a9136262beded0d9a2d94ff3bceccbc5cf913353
jwilliamson1 <joseph.williamson15@gmail.com>
2017-03-24 16:43:06 -0400

    add insertR* and occur*

--- SchemerNums.rkt
+++ SchemerNums.rkt
@@ -304,27 +304,33 @@
   (lambda (a l)
     (cond
       [(null? l)(quote())]
-      [(atom? l)
+      [(atom? (car l))
        (cond
-         [(eq? l a)'() ]
-         [else l])]
-      [(lat? l)
-       (cond         
-         ;returns the cdr removing desired param
-         [(eq? a (car l))(cdr l)]
-         ;returns the lat unchanged
+         [(eq? (car l) a) (rember* a (cdr l)) ]
          [else (cons (car l)(rember* a (cdr l)))])]
-      
-      [else
-       (cond       
-         ;recurs
-         [else (cons (rember* a (car l))(rember* a (cdr l)))])])))
+      [else(cons (rember* a (car l))(rember* a (cdr l)))])))
+;(rember* 'sauce '(((tomato sauce))((bean) sauce)(and ((flying)) sauce)) )
+;(rember* 'cup '((coffee) cup ((tea) cup)(and (hick)) cup))
 
+(define insertR*
+  (lambda (new old l)
+    (cond
+      [(null? l)(quote())]
+      [(atom? (car l))
+       (cond
+         [(eq? (car l) old)(cons old (cons new (insertR* new old (cdr l))))]
+         [else(cons(car l)(insertR* new old (cdr l)))]
+         )]
+      [else(cons(insertR* new old (car l))(insertR* new old (cdr l)))])))
+;(insertR* 'roast 'chuck '(((how much (wood))could((a (wood) chuck))(((chuck)))(if (a) ((wood chuck)))could chuck wood) ))
 
-
-(rember* 'sauce '(((tomato sauce))
-((bean) sauce)
-(and ((flying)) sauce)) )
-
-(rember* 'cup '((coffee) cup ((tea) cup)
-(and (hick)) cup)) 
+(define occur*
+  (lambda (a l)
+    (cond
+      [(null? l) 0]
+      [(atom? (car l))
+       (cond
+         [(eq? (car l) a)(add1 (occur* a (cdr l)))]
+         [else (occur* a (cdr l))])]
+      [else(add(occur* a (car l))(occur* a (cdr l)))])))
+(occur* 'banana '((banana)(split ((((banana ice)))(cream (banana))sherbet))(banana)(bread)(banana brandy)) )
