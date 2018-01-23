e177f7d792a9b06c92504e564e465bb95f1267b2
jwilliamson1 <joseph.williamson15@gmail.com>
2018-01-23 06:29:46 -0500

    workin

--- symbols.rkt
+++ symbols.rkt
@@ -40,6 +40,9 @@
 
 (car '(quote a))
 
+(define (=number? exp num)
+  (and (number? exp) (= exp num)))
+
 (define (variable? x) (symbol? x))
 
 (define (same-variable? v1 v2)
@@ -47,13 +50,37 @@
        (variable? v2)
        (eq? v1 v2)))
 
+(define (make-exponentiation b e)
+  (cond((and (=number? b 0)(=number? e 0))1)
+       ((=number? e 0) 1)
+       ((=number? e 1) b)
+       ((and (number? a1) (number? a2)) 
+        (expt a1 a2))
+       (else (list '^ b e))))
+
 (define (make-sum a1 a2)
-  (cond ((==number? a1 0) a2)
+  (cond ((=number? a1 0) a2)
         ((=number? a2 0) a1)
         ((and (number? a1) (number? a2)) 
          (+ a1 a2))
         (else (list '+ a1 a2))))
-(define (make-product m1 m2) (list '* m1 m2))
+
+(define (make-product m1 m2)
+  (cond ((or (=number? m1 0) 
+             (=number? m2 0)) 
+         0)
+        ((=number? m1 1) m2)
+        ((=number? m2 1) m1)
+        ((and (number? m1) (number? m2)) 
+         (* m1 m2))
+        (else (list '* m1 m2))))
+
+(define (exponentiation? x)
+  (and (pair? x) (eq? (car x) '^)))
+
+(define (base e) (cadr e))
+
+(define (exponent e) (caddr e))
 
 (define (sum? x)
   (and (pair? x) (eq? (car x) '+)))
