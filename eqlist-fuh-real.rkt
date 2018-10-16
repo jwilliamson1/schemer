f7c32cc30ae2e9695bcf03cac0c0aea223389b50
jwilliamson1 <joseph.williamson15@gmail.com>
2018-10-15 22:12:33 -0400

    simplify eqlist and create equal?

--- eqlist-fuh-real.rkt
+++ eqlist-fuh-real.rkt
@@ -11,19 +11,24 @@
         (else (eq? x1 x2))))
 
 (define eqlist?
-(lambda ( l1 l2)
-(cond
-((and (null? l1 ) (null? l2)) #t)
-((or (null? l1) (null? l2)) #f)
-((and (atom? (car l1 ))(atom? (car l2)))
- (and (eqan? (car l1 ) (car l2))
-      (eqlist? (cdr l1 ) (cdr l2))))
-((or (atom? (car l2))(atom? (car l2))) #f)
-(else
- (and ( eqlist? ( car l1 ) ( car l2))
-      ( eqlist? ( cdr l1 ) ( cdr l2))))))) 
+  (lambda (l1 l2)
+    (cond
+      ((and (null? l1 )(null? l2)) #t)
+      ((or (null? l1)(null? l2)) #f)   
+      (else
+       (and (equal? (car l1) (car l2))
+            (equal? (cdr l1) (cdr l2)))))))
+
+(define equal?
+  (lambda (s1 s2)
+    (cond((and (atom? s1)(atom? s2))
+          (eqan? s1 s2))
+         ((or (atom? s1)(atom? s2)) #f)
+         (else (eqlist? s1 s2)))))
 
 (display "should be #t")(eqlist? '(((test)) test) '(((test)) test))
+(display "should be #t")(eqlist? '(test (test)) '(test (test)))
+(display "should be #f")(eqlist? '(test (test)) '(test test))
 (display "should be #t")(eqlist? '() '())
 (display "should be #f")(eqlist? '(a) '())
 (display "should be #f")(eqlist? '() '(a))
@@ -32,4 +37,31 @@
 (display "should be #t")(eqlist? '(a b) '(a b))
 (display "should be #t")(eqlist? '(strawberry ice cream) '(strawberry ice cream))
 (display "should be #f")(eqlist? '((test) test2) '(test test2))
-(display "should be #f")(eqlist? '(test test2) '((test) test2))
+;(display "should be #f")(eqlist? '(test test2) '((test) test2))
+
+
+
+(equal? '(a b)'(a b))
+(equal? '(a b)'(a b c))
+(equal? '(a bc)'(a b))
+(equal? 'a'(a b))
+(equal? '(a b) 'b)
+(equal? '(a (b))'(a (b)))
+(equal? '((a) b)'((a) b c))
+(equal? '(a bc)'(a b))
+(equal? 'a'(a b))
+(equal? '(a b) 'b)
+(equal? '() '())
+(equal? 'b '())
+(equal? '(a b) '())
+
+(define rember
+  (lambda (s l)
+    (cond ((null? l) '())
+          ((equal? (car l) s)
+           (rember s (cdr l)))
+          (else (cons (car l)(rember s (cdr l)))))))
+
+(rember 'a '(a b c))
+(rember '(a b c) '(z b (a b c) x 4))
+           
