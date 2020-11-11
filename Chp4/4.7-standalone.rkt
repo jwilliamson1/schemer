a478eaa6e0a8006fdd2bfe13e409fe7595ad85fc
Joseph Williamson <jwilliamson@kbra.com>
2020-11-11 17:52:07 -0500

    start 4.8 in 4.7

--- Chp4/4.7-standalone.rkt
+++ Chp4/4.7-standalone.rkt
@@ -32,4 +32,24 @@
           (else (cons (make-lambda vars body) exps)))))
 
 (define let1 '(let ((x (+ 5 5)))(+ x 1)))
-(define let2 '(let ((x (+ free 2))(y (* free2 free3))) (+ 1 x y)))
+
+(define let2 '(let ((x (+ free 2))
+                    (y (* free2 free3))) (+ 1 x y)))
+
+(define (last-var? vars) (null? (cdr vars)))
+
+(define (let*->combination let-block)
+
+  (define (let*-iter real-body vars exps)
+    (if (last-var? vars)
+         (cons (make-lambda vars real-body) exps)
+         (cons (make-lambda (list (car vars))
+                            (let*-iter real-body (cdr vars) (cdr exps)))
+                            (list (car exps)))))
+  
+   (let ((var-list (let-variables let-block))
+        (exps-list (let-expressions let-block))
+        (body (let-body let-block)))
+     (let*-iter body var-list exps-list)))
+     
+         
